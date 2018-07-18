95985e4b859129eb7e6ee125114d9f150af93bbf
cindy <cindy.wangxx@dadaabc.com>
2018-07-18 18:25:27 +0800

    element-ui

--- src/components/test/todoList.vue
+++ src/components/test/todoList.vue
@@ -1,14 +1,23 @@
 <template>
+<html>
+    <head>
+        <link rel="stysheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
+    </head>
+    <body>
     <div class="todoList">
         <h1>{{title}}</h1>
         <input type="text" class="addTodo" id="addTodo" v-model="newTodo" @keyup.enter="add">
         <ul v-if="list !== []">
             <li v-for="li in list" :class="{finish:li.value}" @click="cancle">
-                <span>{{li.event}}</span><span v-if="li.value === 0">未完成</span><span v-else>已完成</span>
+                <el-button>{{li.event}}</el-button><span v-if="li.value === 0">未完成</span><span v-else>已完成</span>
             </li>
         </ul>
     </div>
+    </body>
+</html>
 </template>
+<script src="https://unpkg.com/element-ui/lib/index.js"></script>
+
 <script>
 export default {
     data () {
